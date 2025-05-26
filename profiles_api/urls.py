e312bed55a22e94695e100ba0419590b43b1e704
Zimp <zimplitech@outlook.com>
2025-05-26 14:17:11 +0700

    Added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,9 @@ from profiles_api import views
 # To register ViewSet, need to do it thru Router
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, base_name="hello-viewset")
+router.register(
+    "profile", views.UserProfileViewSet
+)  # No need to specify base_name because in views.py, we have queryset object and Django framework can know by itself!
 
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),  # This is to register API Views
